c9d474117270a6d7d626abe15d2ad6e17095c2c8
anmarvin <minwolf.999@gmail.com>
2024-08-23 15:41:23 +0200

    better placement for the result message for the form and verification not 2 similar students in a same class

--- handler/addEleve.go
+++ handler/addEleve.go
@@ -57,6 +57,17 @@ func AddEleve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	students, _ := controller.SelectStudents("students", []string{"name", "class", "year", "teacherId"}, name, class, year, teacher.Id)
+	if len(students) != 0 {
+		// Write someone comming to the addEleve route in the log file
+		file, _ := os.OpenFile(structure.App.LogPath[1:], os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		file.WriteString(fmt.Sprintf("%s [%s] %s - /saisir/addEleve route - %s - error: %s\n", time.Now().Format("2006-01-02 15:04:05"), r.RemoteAddr, r.Method, id, "Il y a déjà un élève portant ce nom dans cette classe"))
+		file.Close()
+
+		structure.Tpl.ExecuteTemplate(w, "addEleve.html", structure.Result{Class: strings.Split(teacher.Class, " | "), Error: "Il y a déjà un élève portant ce nom dans cette classe"})
+		return
+	}
+
 	// Add the new student in the BDD
 	err = controller.AddNew("students", id, name, class, year, teacher.Id)
 	if err != nil {
